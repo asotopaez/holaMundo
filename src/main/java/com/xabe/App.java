be914911a480c9a7a0aa81dc52568e9758da10fa
asotopaez <asotopaez@gmail.com>
2020-05-16 13:03:39 -0500

    nice hola mundo

--- src/main/java/com/xabe/App.java
+++ src/main/java/com/xabe/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hola Mundo desde Jenkins" );
+        System.out.println( "Hola Mundo desde Jenkins2" );
     }
 }
